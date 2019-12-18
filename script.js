31c03cbfdc43311649fdb22417d8823accc28d7b
maruscak.jan <maruscak.jan@ssakhk.cz>
2019-12-18 22:18:08 +0100

    Breaking things

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
 window.onscroll = function() {OnScroll()};
+var scroll = window.requestAnimationFrame ||
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+
+function loop() {
+
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('visible');
+    } else {
+      element.classList.remove('visible');
+    }
+  });
+
+  scroll(loop);
+}
+
+// loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
 
 function OnScroll() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    document.getElementById("navbar").className = "scrolled";
-    document.getElementById("navbarUl").className = "scrolled-ul";
+    // document.getElementById("navbar").className = "scrolled";
+    document.getElementById("navbar").classList.add("scrolled");
+    document.getElementById("navbarUl").classList.add("scrolled-ul");
   }
   else{
-    document.getElementById("navbar").className = "";
-    document.getElementById("navbarUl").className = "";
+    // document.getElementById("navbar").className = "";    
+    document.getElementById("navbar").classList.remove("scrolled");
+    document.getElementById("navbarUl").classList.remove("scrolled-ul");
+    // document.getElementById("navbarUl").className = "";
   }
+}
+
+function ToggleMobile(){
+  document.getElementById('navbarUl').classList.toggle('open');
 }
